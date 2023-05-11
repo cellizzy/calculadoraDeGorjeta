038a5e1367f2f58a4a9996bb7bd2e36d6609631e
Aluno <ninaazevedo0202@gmail.com>
2023-05-11 17:17:19 -0300

    11/05

--- app/src/main/java/com/example/calculador/MainActivity.kt
+++ app/src/main/java/com/example/calculador/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -21,11 +22,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.calculador.ui.theme.CalculadorTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,47 +50,65 @@ class MainActivity : ComponentActivity() {
 fun Appcalculadora(){
 
     var valorEntrada by remember { mutableStateOf("") }
+    var  gorjeta by remember { mutableStateOf(0.0) }
+    var percentagemGorjeta = 15.0
 
+    gorjeta= CalcularGorjeta(valorEntrada,percentagemGorjeta)
 
-    Surface(
+Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        color = Color.LightGray
     ) {
 
-    }
 
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Calcular Gorjeta" ,
-            modifier = Modifier.padding( top =25.dp),
-            fontSize = 25.sp,
-            textAlign = TextAlign.Center,
-            color = Color.Black
-
-     )
-        TextField(
-            value = valorEntrada ,
-            label = {
-                   Text(
-                       text ="Valor de entrada" )
-            },
-            onValueChange = {valorEntrada= it },
+        Column(
             modifier = Modifier
-                .padding( top =25.dp )
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = "Calcular Gorjeta" ,
+                modifier = Modifier.padding( top =25.dp),
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
 
-        )
+         )
+            TextField(
+                value = valorEntrada,
+                label = { Text(text = "Valor de Entrada")
+                        },
+                onValueChange = {valorEntrada = it},
+                modifier = Modifier.padding(top = 25.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+             TextField(
+                value = percentagemGorjeta,
+                label = { Text(text = "Valor de Entrada")
+                },
+                onValueChange = {percentagemGorjeta = it},
+                modifier = Modifier.padding(top = 25.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
 
-        Text(
-            text ="valor da Gorjeta:" ,
-        modifier = Modifier.padding( top =25.dp),
-        fontSize = 25.sp,
-        textAlign = TextAlign.Center,
-        color = Color.Black)
+            Text(
+                text ="valor da Gorjeta: ${NumberFormat.getCurrencyInstance().format(gorjeta)}",
+                modifier = Modifier.padding( top =25.dp),
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
     }
+}
+fun CalcularGorjeta(
+    valorEntrada:String,
+    percentagemGorjeta:Double
+):Double{
+        return (valorEntrada.toDoubleOrNull()?:0.0)*percentagemGorjeta/100
 }
