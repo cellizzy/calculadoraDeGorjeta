b1874e996a0acf1ac537acd7039219762ebc8554
Aluno <ninaazevedo0202@gmail.com>
2023-05-11 17:22:29 -0300

    11/05

--- app/src/main/java/com/example/calculador/MainActivity.kt
+++ app/src/main/java/com/example/calculador/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -20,9 +21,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,7 +55,8 @@ fun Appcalculadora(){
 
     var valorEntrada by remember { mutableStateOf("") }
     var  gorjeta by remember { mutableStateOf(0.0) }
-    var percentagemGorjeta = 15.0
+    var percentagemGorjeta by remember { mutableStateOf("") }
+    val focusManager= LocalFocusManager.current
 
     gorjeta= CalcularGorjeta(valorEntrada,percentagemGorjeta)
 
@@ -77,21 +82,24 @@ Surface(
                 fontWeight = FontWeight.Bold
 
          )
-            TextField(
+
+            CampoTextoEditavel(
                 value = valorEntrada,
-                label = { Text(text = "Valor de Entrada")
-                        },
-                onValueChange = {valorEntrada = it},
-                modifier = Modifier.padding(top = 25.dp),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                label ="valor de entrada" ,
+                onValueChange = {valorEntrada=it},
+                imeAction = ImeAction.Next,
+                keyboardActions = KeyboardActions(
+                    onNext = {focusManager.moveFocus(FocusDirection.Down)}
+                )
             )
-             TextField(
+            CampoTextoEditavel(
                 value = percentagemGorjeta,
-                label = { Text(text = "Valor de Entrada")
-                },
-                onValueChange = {percentagemGorjeta = it},
-                modifier = Modifier.padding(top = 25.dp),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                label ="% Gorjeta" ,
+                onValueChange = {percentagemGorjeta=it},
+                imeAction = ImeAction.Done,
+                keyboardActions = KeyboardActions(
+                    onNext = {focusManager.clearFocus()}
+                )
             )
 
             Text(
@@ -108,7 +116,29 @@ Surface(
 }
 fun CalcularGorjeta(
     valorEntrada:String,
-    percentagemGorjeta:Double
+    percentagemGorjeta:String
 ):Double{
-        return (valorEntrada.toDoubleOrNull()?:0.0)*percentagemGorjeta/100
+        return (valorEntrada.toDoubleOrNull()?:0.0)*(percentagemGorjeta.toDoubleOrNull()?:0.0)/100
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CampoTextoEditavel(
+    value: String,
+    label:String,
+    onValueChange:(String)->Unit,
+    imeAction: ImeAction,
+    keyboardActions: KeyboardActions
+){
+    TextField(
+        value = value,
+        label={ Text(text = label)},
+        onValueChange = onValueChange,
+        modifier = Modifier.padding(top = 25.dp),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = imeAction
+        ),
+        keyboardActions = keyboardActions
+    )
 }
